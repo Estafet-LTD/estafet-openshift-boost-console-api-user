29a97e0d4a7f6b157572ab9e2269f8065900fbd6
mike2906 <mike.ruse90@gmail.com>
2020-03-18 13:51:17 +0200

    https://trello.com/c/I7uOVlOt
    Updating to use messages lib user

--- src/main/java/com/estafet/openshift/boost/console/api/user/controller/UserController.java
+++ src/main/java/com/estafet/openshift/boost/console/api/user/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estafet.openshift.boost.commons.lib.model.API;
-import com.estafet.openshift.boost.console.api.user.model.User;
+import com.estafet.openshift.boost.messages.users.User;
 import com.estafet.openshift.boost.console.api.user.service.UserService;
 
 @RestController
--- src/main/java/com/estafet/openshift/boost/console/api/user/service/UserService.java
+++ src/main/java/com/estafet/openshift/boost/console/api/user/service/UserService.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.estafet.openshift.boost.console.api.user.model.User;
 import com.estafet.openshift.boost.console.api.user.openshift.OpenShiftClient;
 import com.openshift.restclient.model.user.IUser;
+import com.estafet.openshift.boost.messages.users.User;
 
 @Service
 public class UserService {
